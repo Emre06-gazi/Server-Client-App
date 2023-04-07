3e9614d36a57e3478d8643ea58b826d23a89046e
Emre06-gazi <75805390+Emre06-gazi@users.noreply.github.com>
2023-04-07 13:23:13 +0300

    Add files via upload

---
+++ Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ClientUygulamasi
+{
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private TcpListener listener;
+        private bool isListening;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectToServer();
+            ReceiveResponseFromServerAsync();
+            GetInitialValuesFromServer();
+        }
+
+        private void ConnectToServer()
+        {
+            try
+            {
+                // Server IP adresi ve port numarasını buraya girin
+                client = new TcpClient("127.0.0.1", 8888);
+                stream = client.GetStream();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Server'a bağlanırken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void GetInitialValuesFromServer()
+        {
+            // Server'dan lamba ve text değerlerini alıp ekranda göster
+            string initialData = SendMessageToServer("INITIAL");
+            string[] values = initialData.Split('|');
+            if (values.Length == 2)
+            {
+                // Lamba durumunu güncelle
+                bool isLampOn = bool.Parse(values[0]);
+                UpdateLampButton(isLampOn);
+
+                // Text değerini güncelle
+                string textValue = values[1];
+                UpdateTextButton(textValue);
+            }
+        }
+
+        private void LampButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Lamba butonuna basıldığında server'a mesaj gönder
+                string message = "LAMP|" + (LampButton.Background == Brushes.Yellow ? "OFF" : "ON");
+                string response = SendMessageToServer(message);
+
+                if (response.Contains("|"))
+                {
+                    UpdateLampButton(response.Split('|')[1].ToUpper() == "ON" ? true : false);
+                }
+                else
+                {
+                    UpdateLampButton(response == "ON" ? true : false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lamba butonuna basıldığında bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void UpdateLampButton(bool isLampOn)
+        {
+            try
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    // Lamba durumuna göre lamba butonunun rengini güncelle
+                    LampButton.Background = isLampOn ? Brushes.Yellow : Brushes.Gray;
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lamba butonunun rengi güncellenirken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void TextButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Text butonuna basıldığında server'a mesaj gönder
+            string message = "TEXT";
+            string response = SendMessageToServer(message);
+            // Server'dan "TEXT|değer" şeklinde yanıt gelirse text değerini güncelle
+            UpdateTextButton(response);
+        }
+
+        private void UpdateTextButton(string response)
+        {
+            // Text değerini güncelle
+            try
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    // Texte göre text butonunun değerini güncelle
+                    TextButton.Content = response;
+                });
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private string SendMessageToServer(string message)
+        {
+            try
+            {
+                // Server'a mesaj gönder
+                byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                //Server'dan yanıt al
+                //data = new byte[256];
+                //string response = "";
+                //int bytes = stream.Read(data, 0, data.Length);
+                //response = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Server ile iletişim kurulurken bir hata oluştu: " + ex.Message);
+                return "";
+            }
+        }
+        Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+
+        private void ReceiveResponseFromServerAsync()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        byte[] bufferToRead = new byte[256];
+                        int len = stream.Read(bufferToRead, 0, bufferToRead.Length);
+                        if (len > 0)
+                        {
+                            byte[] data = new byte[256];
+                            string result = System.Text.Encoding.UTF8.GetString(bufferToRead, 0, len);
+                            //if (result.Contains("TEXT"))
+                            //{
+                            dispatcher.Invoke(() =>
+                            {
+                                if (result.Contains("|"))
+                                {
+                                    UpdateLampButton(result.Split('|')[1].ToUpper() == "ON" ? true : false);
+                                }
+                                else
+                                {
+                                    UpdateLampButton(result.ToUpper() == "ON" ? true : false);
+                                }
+                            });
+                            
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    { }
+                }
+            });
+        }
+
+        private Brush originalLampButtonBackground;
+        private void LampButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            originalLampButtonBackground = LampButton.Background;
+        }
+
+
+    }
+}
---
+++ Server/Server/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Server
+{
+    public partial class MainWindow : Window
+    {
+        // Bağlı client soketlerinin tutulacağı liste
+        private List<Socket> _clientSockets = new List<Socket>();
+
+        // Server soketi
+        private Socket _serverSocket;
+
+        // Verilerin alınacağı buffer boyutu
+        private byte[] _buffer = new byte[1024];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Server'ı başlat
+            SetupServer();
+        }
+
+        private void SetupServer()
+        {
+            try
+            {
+                // Server soketini oluştur ve istemcilerin bağlanacağı portu belirle
+                _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8888));
+
+                // Dinleme moduna al
+                _serverSocket.Listen(5);
+
+                // İstemci bağlandığında tetiklenecek metod belirlenir ve başlatılır
+                _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+                // Server başarıyla başlatıldı mesajı yazdırılır
+                statusLabel.Content = "Server başlatıldı.";
+            }
+            catch (Exception ex)
+            {
+                // Server başlatılırken bir hata oluşursa mesaj kutusu ile kullanıcıya bilgi verilir
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            // Yeni bir istemci bağlandığında tetiklenecek metod
+            Socket socket = _serverSocket.EndAccept(ar);
+
+            // Bağlanan istemci soketi listeye eklenir
+            _clientSockets.Add(socket);
+
+            // Bağlanan istemci sayısı gösterilir
+            statusLabel.Dispatcher.Invoke(() => statusLabel.Content = $"Bağlanılan Clientlar: {_clientSockets.Count}");
+
+            // Veri almak için sokete dinleme işlemi başlatılır
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+            // Yeni bir istemci bağlantısı için dinleme modu tekrar aktif edilir
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            // İstemciden veri alındığında tetiklenecek metod
+            Socket socket = (Socket)ar.AsyncState;
+
+            try
+            {
+                // Gelen verinin boyutu alınır
+                int received = socket.EndReceive(ar);
+
+                // Gelen veri buffer'a kopyalanır
+                byte[] dataBuf = new byte[received];
+                Array.Copy(_buffer, dataBuf, received);
+
+                // Gelen veri ASCII kodlaması ile stringe çevrilir
+                string text = Encoding.ASCII.GetString(dataBuf);
+
+                // Gelen veri, lamba aç/kapa işlemi ise
+                if (text.StartsWith("LAMP"))
+                {
+                    // Durum metni ayrıştırılır
+                    string state = text.Split('|')[1];
+                    // Eğer durum "Açık" veya "Kapalı" ise
+                    if (state == "ON" || state == "OFF")
+                    {
+                        // Lamba görseli değiştirilir ve değişiklik tüm istemcilere gönderilir
+                        LambaImage.Dispatcher.Invoke(() => LambaImage.Source = state == "ON" ? new BitmapImage(new Uri("LampOn.jpg", UriKind.Relative)) : new BitmapImage(new Uri("LampOff.jpg", UriKind.Relative)));
+                        BroadcastMessage(text);
+                    }
+                }
+                // Gelen veri, text kutusunda gösterilecek metin ise
+                else 
+                {
+                    // Gösterilecek metin ayrıştırılır
+                    string value = text.Replace("Text:", "");
+                    // Metin kutusu güncellenir ve değişiklik tüm istemcilere gönderilir
+                    TextInput.Dispatcher.Invoke(() => TextInput.Text = value);
+                    BroadcastMessage(text);
+                }
+                // Veri alımı devam eder
+                socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            }
+            // Hata durumunda
+            catch (Exception ex)
+            {
+                // Hata mesajı gösterilir
+                Console.WriteLine(ex.Message);
+                // Socket kapatılır ve listeden çıkarılır
+                socket.Close();
+                _clientSockets.Remove(socket);
+                // Bağlı istemci sayısı güncellenir
+                statusLabel.Dispatcher.Invoke(() => statusLabel.Content = $"Bağlanılan Clientlar: {_clientSockets.Count}");
+            }
+        }
+
+        // Verilen iletiyi ASCII kodlaması kullanarak tüm soketlere gönderir.
+        private void BroadcastMessage(string message)
+        {
+            // İletiyi ASCII kodlaması kullanarak byte dizisine dönüştürür.
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+            // Her bir soket nesnesi için döngü oluşturulur.
+            foreach (Socket socket in _clientSockets)
+            {
+                // Byte dizisi gönderilir.
+                socket.Send(buffer);
+            }
+        }
+
+
+        private void LambaButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Lamba durumu değiştirilir.
+
+
+            var durum = LambaImage.Source;
+
+            var ImageUrl = new Uri($"{durum}");
+
+            BitmapImage image = new BitmapImage(ImageUrl);
+
+          
+
+            string state = image.UriSource.AbsolutePath.EndsWith("LampOff.jpg") ? "On" : "Off";
+
+            LambaImage.Source = new BitmapImage(new Uri($"pack://application:,,,/Lamp{state}.jpg"));
+
+            // Durum mesajı gösterilir ve tüm clientlara durum bildirimi yapılır.
+
+
+            string statusMessage = $"{state}";
+
+            StatusMessage.Text = statusMessage;
+            StatusMessage.Visibility = Visibility.Visible;
+            BroadcastMessage(statusMessage);
+        }
+       
+
+        private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Text değiştiğinde son değer alınır ve tüm clientlara gönderilir.
+            string text = TextInput.Text;
+            BroadcastMessage(text);
+        }
+
+        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Rastgele bir sayı oluşturulur ve text alanına yazılır.
+            Random random = new Random();
+            int randomNumber = random.Next(10000);
+            TextInput.Text = randomNumber.ToString();
+
+            // Durum mesajı gösterilir ve tüm clientlara sayı gönderimi yapılır.
+            string statusMessage2 = $"TEXT {randomNumber}";
+            StatusMessage2.Text = statusMessage2;
+            StatusMessage2.Visibility = Visibility.Visible;
+            BroadcastMessage(statusMessage2);
+        }
+    }
+}
